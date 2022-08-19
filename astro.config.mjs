41e43cf47b2a8415cac5db64b3c710de84a50439
ndebg <dev@ndebg.com>
2022-08-19 21:25:01 +0300

    remote netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
 import mdx from '@astrojs/mdx';
 import tailwind from "@astrojs/tailwind";
-import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ndebg.github.io',
-  base: '/ndebgcom',
   sitemap: true,
-  integrations: [mdx(), tailwind()],
-  output: 'server',
-  adapter: netlify(),
+  integrations: [mdx(), tailwind()]
 });
