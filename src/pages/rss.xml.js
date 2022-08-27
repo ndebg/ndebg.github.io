f01a96c3a746928ef6a2fee4e7c086094d5d7ad9
ndebg <dev@ndebg.com>
2022-08-27 14:52:34 +0300

    rss: fix rss.xml contents. Fixes #3

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,13 @@
 import rss from '@astrojs/rss';
 
+const postImportResult = import.meta.glob('./**/*.mdx', { eager: true });
+const posts = Object.values(postImportResult);
+
+const nonDraftPosts = posts.filter((post) => !post.frontmatter.draft);
+const nonDraftSortedPosts = nonDraftPosts.sort((a, b) => {
+  return new Date(b.frontmatter.pubDate) - new Date(a.frontmatter.pubDate)
+})
+
 export const get = () => rss({
     title: 'Преживявания близки до смъртта',
     description: 'Преживявания близки до смъртта',
@@ -9,7 +17,13 @@ export const get = () => rss({
     // list of `<item>`s in output xml
     // simple example: generate items for every md file in /src/pages
     // see "Generating items" section for required frontmatter and advanced use cases
-    items: import.meta.glob('./**/*.md'),
+    // items: import.meta.glob(['./*.mdx', './pages/*.mdx', './nde/*.mdx', './nde/bg/*.mdx']),
+    items: nonDraftSortedPosts.map((post) => ({
+      link: post.url,
+      title: post.frontmatter.title,
+      description: post.frontmatter.description,
+      pubDate: post.frontmatter.pubDate,
+    })),
     stylesheet: '/rss/styles.xsl',
     // (optional) inject custom xml
     customData: `<language>bg</language>`,
